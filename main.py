9627e5d28079658656abdeebdc57e0057536c463
Dave <davepeck@gmail.com>
2025-10-31 10:50:31 -0700

    In progress: run across thousands of feeds

--- main.py
+++ main.py
@@ -1,5 +1,73 @@
+from pathlib import Path
+
+import click
+
+from feedmodels.rss import parse_rss
+
+
+@click.group()
 def main():
-    print("Hello from feedmodels!")
+    pass
+
+
+def _is_feed_any_rss(feed: str) -> bool:
+    return "<rss" in feed
+
+
+@main.command()
+# Add a feed_path argument to the command
+@click.argument("feed_path", type=click.Path(exists=True))
+def parse(feed_path: Path):
+    """Parse the RSS feed at the given FEED_PATH and print the titles of the items."""
+
+    with open(feed_path, "r", encoding="utf-8") as f:
+        feed_content = f.read()
+
+    if not _is_feed_any_rss(feed_content):
+        click.echo(
+            "The provided file does not appear to be a valid RSS feed.", err=True
+        )
+        return
+
+    feed = parse_rss(feed_content)
+
+    click.echo(f"Feed Title: {feed.channel.title}")
+    click.echo("Items:")
+    for item in feed.channel.items:
+        click.echo(f"- {item.pub_date}: {item.title}")
+
+
+@main.command()
+@click.argument("feed_dir_path", type=click.Path(exists=True, dir_okay=True))
+def parse_dir(feed_dir_path: Path):
+    """Parse all RSS feeds in the given FEED_DIR_PATH and print the titles of the items."""
+
+    feed_dir = Path(feed_dir_path)
+
+    for i, feed_file in enumerate(feed_dir.iterdir()):
+        if feed_file.is_file():
+            with open(feed_file, "r", encoding="utf-8") as f:
+                feed_content = f.read()
+
+            if not _is_feed_any_rss(feed_content):
+                click.echo(
+                    f"[{i + 1}] Skipping {feed_file.name}: Not a valid RSS feed.",
+                    err=True,
+                )
+                continue
+
+            try:
+                feed = parse_rss(feed_content)
+            except Exception as e:
+                click.echo(f"[{i + 1}] Error parsing {feed_file.name}: {e}", err=True)
+                raise
+
+            click.echo(
+                f"\n\n-------\n\n[{i + 1}]\nFeed Title: {feed.channel.title} (from {feed_file.name})"
+            )
+            click.echo("Items:")
+            for item in feed.channel.items:
+                click.echo(f"- {item.pub_date}: {item.title}")
 
 
 if __name__ == "__main__":
